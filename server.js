7beb70e3a1fcaf0ea2fd9aed3449342595137971
Sidhik AR <sidhik64742@gmail.com>
2020-12-11 17:44:36 +0530

    fist commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+// const MongoClient = require('mongodb').MongoClient;
+var app = express();
+// const url = 'mongodb://localhost:27017/';
+
+var customerNo = 1;
+
+app.use(function (req, res, next) {
+  next();
+});
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/index', function (req, res) {
+    console.log("got it");
+});
+console.log('Server listen at port 8000');
+app.listen(8000);
